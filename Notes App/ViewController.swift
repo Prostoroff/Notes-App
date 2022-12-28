9fe9e007fce0097731180f620f7cdd01c01cd633
Ivan Osipov <ivan.loft72@gmail.com>
2022-12-29 03:01:53 +0500

    Initial Commit

---
+++ Notes App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notes App
+//
+//  Created by Иван Осипов on 29.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
